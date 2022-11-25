956ed7ab90958324ca49fcc57f4dcf71d9b2c76a
Jérémy Brialon <jeremy.brialon@emea.merkleinc.com>
2022-11-25 09:48:32 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# Vue 3 + Vite + Replicate.js
+
+For private development and testing, use the lightweight proxy.
+A proxy is necessary to avoid CORS issues with the Replicate HTTP API.
+
+get a token from https://replicate.com/api
+
+`node ./cors-proxy.js`
+`npm run dev`
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,25 @@
+<script>
+import Replicate from "../lib/Replicate.js";
+const replicate = new Replicate({ proxyUrl: "http://localhost:3000/api" });
+
+const version = "3b5c0242f8925a4ab6c79b4c51e9b4ce6374e9b07b5e8461d89e692fd0faa449";
+
+export default {
+  data() {
+    return {
+      msg: "World",
+    };
+  },
+  async mounted() {
+    const helloWorldModel = await replicate.models.get("replicate/hello-world");
+    const helloWorldPrediction = await helloWorldModel.predict({ text: this.msg });
+    this.msg = helloWorldPrediction;
+  },
+};
+</script>
+
+<template>
+  <h1>{{ msg }}</h1>
+</template>
+
+<style scoped></style>
